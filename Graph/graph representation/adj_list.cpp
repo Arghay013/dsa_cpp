58d5b39e4b823f4b697bf0075a3fd8a5e70df26b
Arghay013 <arghay.cse8.bu@gmail.com>
2025-02-10 10:04:15 +0600

    adj_list

--- Graph/graph representation/adj_list.cpp
+++ Graph/graph representation/adj_list.cpp
@@ -1,28 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int main(){
+int main()
+{
     int n,e;
     cin >> n >> e;
-    int adj[n+2][n+2];
-    memset(adj,0,sizeof(adj));
-
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            if(i==j)adj[i][j]=1;
-        }
-        cout<<endl;
-    }
-    for(int i=1;i<=e;i++){
+    int a[n];
+    vector<int> adj_list[n];    // array of vector 
+    while(e--)
+    {
         int a,b;
         cin >> a >> b;
-        adj[a][b]=1;
-        adj[b][a]=1;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);     // undirected
     }
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cout<<adj[i][j]<<" ";
-        }
-        cout<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout << i << " -> ";
+        for(int x : adj_list[i])
+            cout << x << " ";
+        cout << endl;
     }
+    return 0;
 }
